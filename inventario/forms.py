ef8255849a8018bb1e9871a2f3a67a87aba9cef7
Daniel C <danielc@live.cl>
2025-11-30 03:20:11 -0300

    apps usuarios, inventario, y algunas plantillas html

---
+++ inventario/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Producto, Categoria, Etiqueta
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio', 'categoria', 'etiquetas']
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nombre']
+
+
+class EtiquetaForm(forms.ModelForm):
+    class Meta:
+        model = Etiqueta
+        fields = ['nombre']
